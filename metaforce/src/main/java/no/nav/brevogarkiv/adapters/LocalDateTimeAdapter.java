c09ea1a47efead21dc18daa4957f2daaba2d24a6
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2023-05-15 13:45:47 +0200

    La til metaforce intern SOAP tjenestespesifikasjon

---
+++ metaforce/src/main/java/no/nav/brevogarkiv/adapters/LocalDateTimeAdapter.java
@@ -0,0 +1,25 @@
+package no.nav.brevogarkiv.adapters;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDateTime;
+
+/**
+ * Adapter for converting xs:dateTime String to LocalDateTime Note that
+ * xs:dateTime allows for timezone. LocalDateTime is timezone agnostic.
+ * 
+ * @author Joakim Bjørnstad, Visma Consulting
+ */
+public class LocalDateTimeAdapter extends XmlAdapter<String, LocalDateTime> {
+
+	@Override
+	public LocalDateTime unmarshal(String v) throws Exception {
+		return DateTime.parse(v).toLocalDateTime();
+	}
+
+	@Override
+	public String marshal(LocalDateTime v) throws Exception {
+		return v.toDateTime().toString();
+	}
+}
